ee4373d13e928459f59a23bc14e0f2c89e316f87
imamhossain94 <imamagun94@gmail.com>
2021-11-02 23:23:31 +0600

    feat: first commit

---
+++ script.py
@@ -0,0 +1,125 @@
+bi_semester = """01 Sep 2021,20 Sep 2021|Wednesday,Monday|Payment of tuition fees: Ist installment
+07 Sep 2021,07 Sep 2021|Tuesday,Tuesday|Orientation of newly admitted students
+08 Sep 2021,08 Sep 2021|Wednesday,Wednesday|Commencement of class
+30 Sep 2021,30 Sep 2021|Thursday,Thursday|Deadline of course(s) add/drop/semester withdrawal
+01 Oct 2021,07 Oct 2021|Friday,Thursday|Submission of application for tuition fees waiver
+06 Oct 2021,06 Oct 2021|Wednesday,Wednesday|Holiday (Akheri Chahar Somba) *
+11 Oct 2021,11 Oct 2021|Monday,Monday|Declaration of Waiver List
+12 Oct 2021,28 Oct 2021|Tuesday,Thursday|Payment of Tuition Fees: 2nd Installment
+15 Oct 2021,15 Oct 2021|Friday,Friday|Holiday (Durga Puja: Vijaya Dashami) *
+19 Oct 2021,19 Oct 2021|Tuesday,Tuesday|Holiday (Eid-e-Milad-un-Nabi) *
+22 Oct 2021,22 Oct 2021|Friday,Friday|Deadline of submission of student attendance report before Midterm Examination
+26 Oct 2021,26 Oct 2021|Tuesday,Tuesday|Last day of classes before Midterm Examination
+27 Oct 2021,28 Oct 2021|Wednesday,Thursday|Preparatory leave for Midterm Examination
+29 Oct 2021,07 Nov 2021|Friday,Sunday|Midterm Examination (10 Days)
+08 Nov 2021,08 Nov 2021|Monday,Monday|Classes resume after Midterm Examinations
+10 Nov 2021,10 Nov 2021|Wednesday,Wednesday|Deadline of submission of application for registration of Supplementary Midterm Examination
+13 Nov 2021,13 Nov 2021|Saturday,Saturday|Deadline of submission of Midterm Examinationresult
+15 Nov 2021,19 Nov 2021|Monday,Friday|Supplementary Midterm Examination
+17 Nov 2021,17 Nov 2021|Wednesday,Wednesday|Holiday (Fatiha-e-Yajdahm) *
+22 Nov 2021,22 Nov 2021|Monday,Monday|Deadline of submission of Supplementary Midterm Examination result
+09 Dec 2021,24 Dec 2021|Thursday,Friday|Payment of Tuition Fees: Final Installment
+16 Dec 2021,16 Dec 2021|Thursday,Thursday|Holiday (Victory Day)
+23 Dec 2021,23 Dec 2021|Thursday,Thursday|Deadline of submission of student attendance report before Final Examination
+25 Dec2021,25 Dec2021|Saturday,Saturday|Holiday (Christmas Day)
+27 Dec 2021,27 Dec 2021|Monday,Monday|Last day of classes before Final Examination
+28 Dec 2021,29 Dec 2021|Tuesday,Wednesday|Preparatory leave for Final Examination
+30 Dec 2021,08 Jan 2022|Thursday,Saturday|Final Examination (10 Days)
+14 Jan 2022,14 Jan 2022|Friday,Friday|Deadline of submission of Final Grade
+15 Jan 2022,15 Jan 2022|Saturday,Saturday|Day of Final Result publication
+15 Jan 2022,16 Jan 2022|Saturday,Sunday|Semester Break for Faculty Members
+16 Jan 2022,16 Jan 2022|Sunday,Sunday|Deadline of submission of application for registration of Supplementary Final Examination
+09 Jan 2022,20 Jan 2022|Sunday,Thursday|Registration for Spring 2022
+17 Jan 2022,17 Jan 2022|Monday,Monday|Orientation of the Newly Admitted Students, Spring 2022
+18 Jan 2022,18 Jan 2022|Tuesday,Tuesday|Commencement of Classes of Spring 2022
+20 Jan 2022,24 Jan 2022|Thursday,Monday|Supplementary Final Examination of Fall 2021-22
+27 Jan 2022,27 Jan 2022|Thursday,Thursday|Deadline of submission of Supplementary Final Examination result
+28 Jan 2022,28 Jan 2022|Friday,Friday|Day of Supplementary Final Examination result publication"""
+
+tri_semester = """09 Aug 2021,26 Aug 2021|Monday,Thursday|Payment of tuition fees: 1st installment
+15 Aug 2021,15 Aug 2021|Sunday,Sunday|Holiday (National Mourning Day)
+18 Aug 2021,18 Aug 2021|Wednesday,Wednesday|Commencement of Classes to be decided PY Orientation of the Newly Admitted Students
+19 Aug 2021,19 Aug 2021|Tuesday,Tuesday|Holiday (Ashura)*
+30 Aug 2021,30 Aug 2021|Monday,Monday|Holiday (Janmashtami) *
+02 Sep 2021,02 Sep 2021|Thursday,Thursday|Deadline of course(s) add/drop/semester withdrawale
+03 Sep 2021,08 Sep 2021|Friday,Wednesday|Submission of application for tuition fees waiver
+15 Sep 2021,15 Sep 2021|Wednesday,Wednesday|Declaration of Waiver List
+16 Sep 2021,30 Sep 2021|Thursday,Thursday|Payment of Tuition Fees: 2nd Installment
+27 Sep 2021,27 Sep 2021|Monday,Monday|Deadline of submission of student attendance report
+30 Sep 2021,30 Sep 2021|Thursday,Thursday|Last day of classes before Midterm Examination
+01 Oct 2021,01 Oct 2021|Friday,Friday|Preparatory leave for Midterm Examination
+02 Oct 2021,09 Oct 2021|Saturday,Saturday|Midterm Examination (8 Days)
+06 Oct 2021,06 Oct 2021|Wednesday,Wednesday|Holiday (Akheri Chahar Somba) *
+10 Oct 2021,10 Oct 2021|Sunday,Sunday|Classes resume after Midterm Examination
+15 Oct 2021,15 Oct 2021|Friday,Friday|Holiday (Durga Puja: Vijaya Dashami) *
+16 Oct 2021,16 Oct 2021|Saturday,Saturday|Deadline of submission of Midterm Examination
+16 Oct 2021,16 Oct 2021|Saturday,Saturday|Saturday Deadline of submission of application for registration of Supplementary Midterm Examination
+19 Oct 2021,19 Oct 2021|Tuesday,Tuesday|Holiday (Eid-e-Milad-un-Nabi) *
+20 Oct 2021,24 Oct 2021|Wednesday,Sunday|Supplementary Midterm Examination
+27 Oct 2021,27 Oct 2021|Wednesday,Wednesday|Deadline of submission of Supplementary Midterm Examination result
+14 Nov 2021,28 Nov 2021|Sunday,Sunday|Payment of Tuition Fees: Final Installment
+17 Nov 2021,17 Nov 2021|Wednesday,Wednesday|Holiday (Fatiha-e-Yajdahm) *
+21 Nov 2021,27 Nov 2021|Sunday,Saturday|Farewell of Outgoing Students
+24 Nov 2021,24 Nov 2021|Wednesday,Wednesday|Deadline of submission of student attendance report
+27 Nov 2021,27 Nov 2021|Saturday,Saturday|Last day of classes before Final Examination
+28 Nov 2021,28 Nov 2021|Sunday,Sunday|Preparatory leave for Final Examination
+29 Nov 2021,06 Dec 2021|Monday,Monday|Final Examination (8 Days)
+07 Dec 2021,24 Dec 2021|Tuesady,Friday|Registration for Fall 2021-22
+12 Dec 2021,12 Dec 2021|Sunday,Sunday|Deadline of submission of Final Grade
+13 Dec 2021,13 Dec 2021|Monday,Monday|Day of Final Result publication
+13 Dec 2021,13 Dec 2021|Monday,Monday|Deadline of submission of application for registration of Supplementary Final Examination
+13 Dec 2021,14 Dec 2021|Monday,Tuesday|Semester Break for Faculty Members
+18 Dec 2021,18 Dec 2021|Wednesday,Wednesday|Commencement of Classes of Fall 2021-22R
+16 Dec 2021,16 Dec 2021|Thursday,Thursday|Holiday (Victory Day)
+19 Dec 2021,22 Dec 2021|Sunday,Wednesday|Supplementary Final Examination of Summer 2021
+25 Dec 2021,25 Dec 2021|Saturday,Saturday|Holiday (Christmas Day)
+26 Dec 2021,26 Dec 2021|Sunday,Sunday|Deadline of submission of Supplementary Final Examination result
+27 Dec 2021,27 Dec 2021|Monday,Monday|Day of Supplementary Final Examination result publication"""
+
+def listItemToActivitiesObj(data):
+    date = data.split('|')[0].split(',')
+    day = data.split('|')[1].split(',')
+    activities = data.split('|')[2]
+    return {
+        "date": {
+            "start": date[0],
+            "end": date[1]
+        },
+        "day": {
+            "start": day[0],
+            "end": day[1]
+        },
+        "activities": activities
+    }
+
+
+
+bi_list = bi_semester.split('\n')
+tri_list = tri_semester.split('\n')
+
+print(len(tri_list))
+
+mode = 1
+
+if mode == 0:
+    data = []
+    for item in bi_list:
+        data.append(listItemToActivitiesObj(item))
+
+    finalData = {
+        "semester": "Fall 2021-22",
+        "semester_type": "Bi-semester",
+        "data": data
+    }
+if mode == 1:
+    data = []
+    for item in tri_list:
+        data.append(listItemToActivitiesObj(item))
+
+    finalData = {
+        "semester": "Summer 2021",
+        "semester_type": "Trimester",
+        "data": data
+    }
+
+print(finalData)    
